ad6b3df9f6211a0507b516cffb827b04f48f7eb2
camel18 <79908416+camel18@users.noreply.github.com>
2021-03-17 20:51:04 +0900

    データベースからのデータ取得方法模索中

--- graduate.py
+++ graduate.py
@@ -1,12 +1,20 @@
+import sqlite3
 import smtplib
 from email.mime.text import MIMEText 
-account = "mzknchi18@gmail.com"
-password = "momo8896"
+
+# con = sqlite3.connect('./necocchi.db')
+# cur = con.cursor()
+# cur.execute("SELECT * FROM users where password = ?")
+# connection.commit()
+# connection.close()
+
+account = "necocchi.help@gmail.com"
+password = "necocchi"
 to_email = "r32.perfume.180sx@gmail.com"
-from_email = "mzknchi18@gmail.com"
-subject = "テストメール"
-message = "テストメール"
-msg = MIMEText(message, "html")
+from_email = "necocchi.help@gmail.com"
+subject = "パスワードのお知らせです"
+body = "あなたのパスワードを通知します。 <br> <br> password <br> <br> このパスワードは大切に保管してください。"
+msg = MIMEText(body, "html")
 msg["Subject"] = subject
 msg["To"] = to_email
 msg["From"] = from_email
